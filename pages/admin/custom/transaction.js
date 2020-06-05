6c3dce7e829ec146cbc75bd5a7f6c972fcec3e8a
wddo <wddoddo@gmail.com>
2020-06-05 18:56:11 +0900

    apply ant-design

---
+++ pages/admin/custom/transaction.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Button } from 'antd'
+
+export default class transaction extends Component {
+  render() {
+    return (
+      <div>
+        <Button>ant design</Button>
+      </div>
+    )
+  }
+}
